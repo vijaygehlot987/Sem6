2662b549b39dfe08debc2720bf47b01cce64a203
Vijay Gehlot <32257800+vijaygehlot987@users.noreply.github.com>
2020-02-03 09:40:48 +0530

    Add files via upload

--- cosine.py
+++ cosine.py
@@ -1,28 +1,26 @@
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
-
-
-
 file1=open("C:/Users/Admin/Documents/a1.txt",'r')
 file2=open("C:/Users/Admin/Documents/a2.txt",'r')
-file3=open("C:/Users/Admin/Documents/a3.txt",'r')
+
  
 # tokenization 
 x = word_tokenize(file1.read())  
 y = word_tokenize(file2.read()) 
-z = word_tokenize(file3.read())
+
   
 # sw contains the list of stopwords 
 sw = stopwords.words('english')  
-l1 =[];l2 =[];l3=[] 
+l1 =[]
+l2 =[]
   
 # remove stop words from string 
 a = {w for w in x if not w in sw}  
 b = {w for w in y if not w in sw}
-c = {w for w in z if not w in sw}
+
   
 # form a set containing keywords of both strings  
-rvector = a.union(b.union(c))  
+rvector = a.union(b)  
 for w in rvector: 
     if w in a: 
         l1.append(1) # create a vector 
@@ -32,14 +30,9 @@ for w in rvector:
         l2.append(1) 
     else: 
         l2.append(0)
-    if w in c: 
-        l3.append(1) 
-    else: 
-        l3.append(0)
-c = 0
-  
+c=0
 # cosine formula  
 for i in range(len(rvector)): 
-        c+= l1[i]*l2[i]*l3[i] 
-cosine = c / float((sum(l1)*sum(l2)*sum(l3))**0.5) 
+        c+= l1[i]*l2[i] 
+cosine = c / float((sum(l1)*sum(l2))**0.5) 
 print("Cosine similarity: ", cosine)
